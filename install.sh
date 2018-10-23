e52ee71663f55082d70502a6b2dbd2243d017aac
Guy Boldon <gb@programmingtolife.com>
2018-10-23 22:53:29 +0200

    add graphql plugin
    /graphql/

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ $wp plugin install custom-post-type-ui --activate
 $wp plugin install wp-rest-api-v2-menus --activate
 $wp plugin install wordpress-seo --activate
 ## For when we want to utilize the GraphQL api -- check for updates
-#$wp plugin install https://github.com/wp-graphql/wp-graphql/archive/v0.0.33.zip --activate
+$wp plugin install https://github.com/wp-graphql/wp-graphql/archive/v0.0.34.zip --activate
 ## Gutenburg is coming:
 # gutenberg-custom-fields
 # 
